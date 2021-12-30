01d5d85eb382853b1c8503cf528ffe22b306d1ce
jenniferksaunders <96429454+jenniferksaunders@users.noreply.github.com>
2021-12-30 17:44:45 +0100

    Add files via upload

---
+++ Part-9/codeChallenge9.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+// 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+
+// 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4
+const players1Final = [...allPlayers, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5
+const { team1, x: draw, team2 } = game.odds;
+//or
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+console.log(team1, draw, team2);
+
+// 6
+const printGoals = function (...playerNames) {
+  console.log(`${playerNames.length} goals were scored by:`);
+  for (let i = 0; i < playerNames.length; i++) {
+    console.log(`${playerNames[i]}`);
+  }
+};
+printGoals(...game.scored);
+
+// 7
+team1 < team2 && team1 < draw && console.log('Team 1 wins');
+team2 < team1 && team2 < draw && console.log('Team 2 wins');
+draw < team1 && draw < team2 && console.log('It was a draw');
